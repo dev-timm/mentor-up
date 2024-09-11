146c4b1701c2fa3fb7a07bdcc4ad6a8953476a2c
dev-timm <timm.kuemmeth@gmail.com>
2024-09-11 14:12:08 +0200

    add Navbar with logo and links

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
+import Navbar from './components/Navbar';
+
 const App = () => {
-  return <h1>MentorUp</h1>;
+  return <Navbar />;
 };
+
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import logo from '../assets/logo.svg';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="nav-container">
+        <div className="nav-main">
+          <img src={logo} className="logo" />
+          <ul className="nav-links">
+            <li className="nav-link">
+              <a href="#">Find a Mentor</a>
+            </li>
+            <li className="nav-link">
+              <a href="#">Become a Mentor</a>
+            </li>
+          </ul>
+        </div>
+
+        <ul className="nav-action">
+          <li className="nav-link">
+            <a href="#">Sign In</a>
+          </li>
+          <li className="btn-b2">
+            <a href="#">Sign Up</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
