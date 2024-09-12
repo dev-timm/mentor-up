d20d6a8ff317e6874b3fe10e68a11683d137c4a5
dev-timm <timm.kuemmeth@gmail.com>
2024-09-12 12:18:39 +0200

    add arrow icon to hero button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import heroImg from '../assets/hero-Img.png';
+import arrowRight from '../assets/icons/arrow-right.svg';
 
 const Hero = () => {
   return (
@@ -11,7 +12,9 @@ const Hero = () => {
           Connect with industry leaders and receive personalized guidance to
           achieve your goals.
         </p>
-        <button className="btn-b1">Find your Mentor</button>
+        <button className="btn-b1">
+          Find your Mentor <img src={arrowRight} alt="right arrow icon" />
+        </button>
       </div>
       <img src={heroImg} className="hero-img" alt="hero image" />
     </section>
