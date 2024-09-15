411bda6e747cef3acced980eb028d84fb7ec76a8
dev-timm <timm.kuemmeth@gmail.com>
2024-09-15 14:31:28 +0200

    add leading companies section to find mentor page

--- src/pages/FindMentor.jsx
+++ src/pages/FindMentor.jsx
@@ -4,28 +4,32 @@ import btnStyles from '../styles/Button.module.css';
 
 import heroImg from '../assets/find-mentor-hero.png';
 import arrowRight from '../assets/icons/arrow-right.svg';
+import LeadingCompanies from '../components/LeadingCompanies';
 
 const FindMentor = () => {
   return (
-    <section className={`${styles.hero}`}>
-      <div className={`${appStyles.container} ${styles.heroContent}`}>
-        <div className={styles.textContainer}>
-          <h1 className={appStyles.centerText}>
-            Find the Perfect Mentor to <br /> Accelerate <span>Success</span>
-          </h1>
-          <p className={`${appStyles.p1} ${appStyles.centerText}`}>
-            Discover mentors with the expertise and insights to <br /> guide
-            your personal and professional growth.
-          </p>
+    <>
+      <section className={`${styles.hero}`}>
+        <div className={`${appStyles.container} ${styles.heroContent}`}>
+          <div className={styles.textContainer}>
+            <h1 className={appStyles.centerText}>
+              Find the Perfect Mentor to <br /> Accelerate <span>Success</span>
+            </h1>
+            <p className={`${appStyles.p1} ${appStyles.centerText}`}>
+              Discover mentors with the expertise and insights to <br /> guide
+              your personal and professional growth.
+            </p>
+          </div>
+          <div className={styles.imgParent}>
+            <img src={heroImg} className={styles.heroImg} alt="hero image" />
+          </div>
+          <a href="/sign-up" className={btnStyles.btnPrimaryLarge}>
+            Find your Mentor <img src={arrowRight} alt="right arrow icon" />
+          </a>
         </div>
-        <div className={styles.imgParent}>
-          <img src={heroImg} className={styles.heroImg} alt="hero image" />
-        </div>
-        <a href="/sign-up" className={btnStyles.btnPrimaryLarge}>
-          Find your Mentor <img src={arrowRight} alt="right arrow icon" />
-        </a>
-      </div>
-    </section>
+      </section>
+      <LeadingCompanies />
+    </>
   );
 };
 export default FindMentor;
